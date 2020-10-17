3b5143ffc14db4614463ae793ae5b639de0b1d07
XPCMasterX <XPCMasterX@gmail.com>
2020-10-17 15:36:38 +0100

    Added electron

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    }); 
+
+    window.setMenuBarVisibility(false);
+    window.loadFile('src/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
